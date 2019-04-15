aee53705a746c9dd8e44de95deb5707a74906960
Renan Brando <reebrando@gmail.com>
2019-04-15 00:54:26 -0300

    Initial dist subtree commit

---
+++ precache-manifest.2d542e6aa4b5d99b75cb1807221a15d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keycash-app/robots.txt"
+  },
+  {
+    "revision": "f95574b0af665c74666c",
+    "url": "/keycash-app/js/chunk-vendors.748b43c8.js"
+  },
+  {
+    "revision": "07ee35c6581113f4f475",
+    "url": "/keycash-app/js/chunk-2d22d746.15bbceb6.js"
+  },
+  {
+    "revision": "d9d3c9408d6448bfe117",
+    "url": "/keycash-app/js/chunk-2d21a3d2.01c7d7bd.js"
+  },
+  {
+    "revision": "8952abfbacbf8543d24b",
+    "url": "/keycash-app/js/app.41ad5c70.js"
+  },
+  {
+    "revision": "b4e6a34f167b61a126234fca91701241",
+    "url": "/keycash-app/index.html"
+  },
+  {
+    "revision": "f95574b0af665c74666c",
+    "url": "/keycash-app/css/chunk-vendors.3ae4e051.css"
+  },
+  {
+    "revision": "8952abfbacbf8543d24b",
+    "url": "/keycash-app/css/app.ac906db3.css"
+  }
+];
